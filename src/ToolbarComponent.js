9377cbe33a23cbcb59e79031ce67c529102b830b
Andrey Lushnikov <lushnikov@chromium.org>
2018-05-07 00:18:45 +0300

    feature: render search box

--- src/ToolbarComponent.js
+++ src/ToolbarComponent.js
@@ -2,5 +2,6 @@ class ToolbarComponent extends EventEmitter {
   constructor() {
     super();
     this.element = document.createElement('toolbar-component');
+    this.element.innerHTML = `<input type='search' autocomplete='off' autocorrect='off' autocapitalize='off' spellcheck='false' placeholder='Start typing to search...'>`;
   }
 }
