83938523ad06058916b9ff40ae800ce713e1f3c1
Andrey Lushnikov <lushnikov@chromium.org>
2018-06-06 22:10:32 -0400

    fix: wait for 'load' event before initializing application
    App navigation is driven by history's 'popstate' event.
    However:
    - 'popstate' event is not fired if the page hasn't fired its 'load'
    event
    - the 'readme' page has a bunch of images that slow down the 'load'
    event
    
    This patch makes sure we don't render any content until the initial
    'load' event is fired. Since page doesn't have much in it, load happens
    pretty fast.

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import {PPTRProduct} from './pptr/PPTRProduct.js';
 
 window.addEventListener('DOMContentLoaded', async () => {
   window.app = new App(document.body);
+  const loadPromise = new Promise(x => window.addEventListener('load', x));
   const product = await PPTRProduct.create();
+  // await 'load' event: app navigation is based on 'popstate' event which doesn't fire
+  // until 'load' is fired.
+  await loadPromise;
   app.initialize(product);
 });
 
