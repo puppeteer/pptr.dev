33f782e1d25c678b532d52eaf3bc0b4ed4cf858a
Andrey Lushnikov <lushnikov@chromium.org>
2019-01-09 12:51:52 -0800

    chore: release v0.3.4+b8397a6

--- docs/sw.js
+++ docs/sw.js
@@ -114,7 +114,7 @@ workbox.precaching.precacheAndRoute([
   },
   {
     "url": "index.js",
-    "revision": "516c1dc89d9ceaed98cea9d32d1fd0f3"
+    "revision": "5bf1c21ff351d484bff769bd99336622"
   },
   {
     "url": "style.css",
